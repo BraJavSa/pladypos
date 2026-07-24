1fc1886e4e804f31493b011d8cd7afffb518f6b4
BraJavSa <brayanjavsa@gmail.com>
2026-07-24 20:06:02 +0200

    chore: remove generated build, install, and log directories for the pladypos package

--- build/pladypos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brayan/ros2_ws/src/pladypos
-# Build directory: /home/brayan/ros2_ws/src/pladypos/build/pladypos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/pladypos/ament_cmake_python/pladypos/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='pladypos',
-    version='1.0.0',
-    packages=find_packages(
-        include=('pladypos', 'pladypos.*')),
-)
